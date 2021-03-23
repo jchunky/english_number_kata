d38497c0ce26cbadc8389f3d27cb8979d1ed9754
Chunky <jasonc@nulogy.com>
2021-03-23 07:34:03 -0400

    refactor

--- english_number.rb
+++ english_number.rb
@@ -1,52 +1,58 @@
-NUMBERS = {
-  1 => "one",
-  2 => "two",
-  3 => "three",
-  4 => "four",
-  5 => "five",
-  6 => "six",
-  7 => "seven",
-  8 => "eight",
-  9 => "nine",
-  10 => "ten",
-  11 => "eleven",
-  12 => "twelve",
-  13 => "thirteen",
-  14 => "fourteen",
-  15 => "fifteen",
-  16 => "sixteen",
-  17 => "seventeen",
-  18 => "eighteen",
-  19 => "nineteen",
-  20 => "twenty",
-  30 => "thirty",
-  40 => "forty",
-  50 => "fifty",
-  60 => "sixty",
-  70 => "seventy",
-  80 => "eighty",
-  90 => "ninety",
-  100 => "hundred",
-  1000 => "thousand",
-  10**6 => "million",
-  10**9 => "billion",
-  10**12 => "trillion",
-}
+class Numeric
+  NUMBERS = {
+    1 => "one",
+    2 => "two",
+    3 => "three",
+    4 => "four",
+    5 => "five",
+    6 => "six",
+    7 => "seven",
+    8 => "eight",
+    9 => "nine",
+    10 => "ten",
+    11 => "eleven",
+    12 => "twelve",
+    13 => "thirteen",
+    14 => "fourteen",
+    15 => "fifteen",
+    16 => "sixteen",
+    17 => "seventeen",
+    18 => "eighteen",
+    19 => "nineteen",
+    20 => "twenty",
+    30 => "thirty",
+    40 => "forty",
+    50 => "fifty",
+    60 => "sixty",
+    70 => "seventy",
+    80 => "eighty",
+    90 => "ninety",
+    100 => "hundred",
+    1000 => "thousand",
+    10**6 => "million",
+    10**9 => "billion",
+    10**12 => "trillion",
+  }
 
-def english_number(number)
-  return "zero" if number == 0
+  def to_english
+    return "zero" if zero?
 
-  say(number)
+    say
+  end
+
+  def say
+    case self
+    when (100..)
+      i, word = NUMBERS.select { |i| i <= self }.max
+      [(self / i).say, word, (self % i).say].compact.join(" ")
+    when (21..)
+      [NUMBERS[truncate(-1)], NUMBERS[self % 10]].compact.join("-")
+    else
+      NUMBERS[self]
+    end
+  end
 end
 
-def say(n)
-  case n
-  when (100..)
-    i, word = NUMBERS.select { |i| i <= n }.max
-    [say(n / i), word, say(n % i)].compact.join(" ")
-  when (21..)
-    [NUMBERS[n.truncate(-1)], NUMBERS[n % 10]].compact.join("-")
-  else
-    NUMBERS[n]
-  end
+def english_number(n)
+  n.to_english
 end
